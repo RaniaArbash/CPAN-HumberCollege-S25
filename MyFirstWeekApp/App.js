9d41edf7effcaf1e3f52d99a5a6bd5f8416eb1f2
rania <raniaarbash@hotmail.com>
2025-05-08 14:26:25 -0400

    Week1Project

---
+++ MyFirstWeekApp/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={ styles.textStyle}>Hello World - Rania Arbash!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    fontSize: 40,
+    color: "red"
+  }
+});
